b94719030997132215519fa64eec98061a0d622f
JohnTing <jot.johnting@gmail.com>
2022-12-21 11:25:38 +0800

    json-stringify-pretty-compact is good

--- src/index.ts
+++ src/index.ts
@@ -1,27 +1,27 @@
 import * as ts from "typescript-json-schema/node_modules/typescript";
 import * as TJS from "typescript-json-schema";
+import stringify from "json-stringify-pretty-compact";
 
+import { EditorView, basicSetup } from "codemirror"
+import { javascript, typescriptLanguage } from "@codemirror/lang-javascript"
 
-import {EditorView, basicSetup} from "codemirror"
-import {javascript, typescriptLanguage} from "@codemirror/lang-javascript"
-
-const editorUpdate = EditorView.updateListener.of(function(e) {
-  if(e.docChanged) {
+const editorUpdate = EditorView.updateListener.of(function (e) {
+  if (e.docChanged) {
     onchange(e.state.doc.toJSON().join("\n"));
   }
 });
 
 let editor = new EditorView({
-  doc:"\n\n\n\n\n\n\n\n\n\n\n\n",
-  extensions: [basicSetup, javascript({typescript:true}), editorUpdate],
-  parent: document.getElementById("inputBox") as HTMLElement, 
+  doc: "\n\n\n\n\n\n\n\n\n\n\n\n",
+  extensions: [basicSetup, javascript({ typescript: true }), editorUpdate],
+  parent: document.getElementById("inputBox") as HTMLElement,
 })
 
 
 let outputEditor = new EditorView({
-  extensions: [basicSetup, javascript({typescript:true})],
-  parent: document.getElementById("outputBox") as HTMLElement, 
-  
+  extensions: [basicSetup, javascript({ typescript: true })],
+  parent: document.getElementById("outputBox") as HTMLElement,
+
 })
 
 function getProgramForText(text: string) {
@@ -29,8 +29,8 @@ function getProgramForText(text: string) {
   const textAst = ts.createSourceFile(dummyFilePath, text, ts.ScriptTarget.Latest);
 
   const options: ts.CompilerOptions = {
-    target:ts.ScriptTarget.ES2016, 
-    module:ts.ModuleKind.CommonJS, 
+    target: ts.ScriptTarget.ES2016,
+    module: ts.ModuleKind.CommonJS,
   };
   const host: ts.CompilerHost = {
     fileExists: filePath => filePath === dummyFilePath,
@@ -43,7 +43,7 @@ function getProgramForText(text: string) {
     getSourceFile: filePath => filePath === dummyFilePath ? textAst : undefined,
     readFile: filePath => filePath === dummyFilePath ? text : undefined,
     useCaseSensitiveFileNames: () => true,
-    writeFile: () => { }, 
+    writeFile: () => { },
   };
   const program = ts.createProgram({
     options,
@@ -58,36 +58,37 @@ function text2JsonSchema(text: string) {
   const program = getProgramForText(text);
   const settings: TJS.PartialArgs = {
     required: true,
-    ignoreErrors:true
+    ignoreErrors: true
   };
 
   // We can either get the schema for one file and one type...
 
   const generator = TJS.buildGenerator(program, settings);
 
-  let result = [] as  {name:string, def:TJS.Definition}[];
+  let result = [] as { name: string, def: TJS.Definition }[];
 
 
-  if(generator) {
+  if (generator) {
     generator.getSymbols().forEach(value => {
-      result.push({name:value.name, def:generator.getSchemaForSymbol(value.name)});
+      result.push({ name: value.name, def: generator.getSchemaForSymbol(value.name) });
     });
   } else {
     alert("buildGenerator fail");
     console.error("buildGenerator fail");
   }
-  
+
   return result;
 }
+
 
 function onchange(text: string) {
   const result = text2JsonSchema(text + "");
   console.log(result);
-  const newtext = result.map(v => `let ${v.name}JsonSchema = ` + JSON.stringify(v.def, null, "    ")).join("\n");
-  outputEditor.dispatch({changes: {from:0, to: outputEditor.state.doc.toString().length, insert: newtext}});
+  const newtext = result.map(v => `const ${v.name}_JsonSchema = ` + stringify(v.def, {})).join("\n");
+  outputEditor.dispatch({ changes: { from: 0, to: outputEditor.state.doc.toString().length, insert: newtext } });
 }
 
 editor.dispatch({
-  changes: {from: 0, insert: "type Person = {\n  name:string,\n  age:number\n}"}
+  changes: { from: 0, insert: "type Person = {\n  id:number,\n  name:string,\n  age:number,\n  work?:string\n}" }
 })
 
